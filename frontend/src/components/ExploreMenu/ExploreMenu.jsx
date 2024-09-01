c64629c05a6bbe565b73dc9509899488b44525b7
Kushal Gowda <136258142+Kushkushal@users.noreply.github.com>
2024-09-01 12:59:51 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,8 +5,7 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore Our Menu</h1>
-        <p className='explore-menu-text'>Find a wide range of options tailored to your taste. Whether you are looking for something light or a full meal, we have got you covered. Browse through our carefully curated dishes and enjoy a flavorful experience with every bite!</p>
+     
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return (
