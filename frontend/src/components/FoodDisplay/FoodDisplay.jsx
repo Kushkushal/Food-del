d38bcb475dcd303af9d316f4f43d8a63d536f4df
Kushal Gowda <136258142+Kushkushal@users.noreply.github.com>
2024-09-02 21:41:42 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,25 +1,37 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
-// eslint-disable-next-line react/prop-types
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
-  return (
-    <div className='food-display' id='food-display'>
-      <div className="food-display-list">
-        {food_list.map((item,index)=>{
-          if (category==="All" || category===item.category){
-            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+const FoodDisplay = ({ category }) => {
+    const { food_list } = useContext(StoreContext);
 
-          }
-           
-        })}
-      </div>
-    </div>
-  )
-}
+    if (!food_list || food_list.length === 0) {
+        return <div className='food-display'>Items loading, please wait</div>;
+    }
 
-export default FoodDisplay
+    return (
+        <div className='food-display' id='food-display'>
+            <div className="food-display-list">
+                {food_list.map((item) => {
+                    if (category === "All" || category === item.category) {
+                        console.log('Rendering item:', item); // Debugging log
+                        return (
+                            <FoodItem
+                                key={item?._id}
+                                id={item?._id}
+                                name={item?.name ?? 'No name'}
+                                description={item?.description ?? 'No description'}
+                                price={item?.price ?? 'No price'}
+                                image={item?.image}
+                            />
+                        );
+                    }
+                    return null;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default FoodDisplay;
